b84fadb5f950014042dac15601c3bcfd0ff338ea
xiaobadao <675499468@qq.com>
2018-06-11 18:06:17 +0800

    iOS 导航
    ios 导航

--- index.js
+++ index.js
@@ -1,89 +1,49 @@
-import {
-    AppRegistry ,
-    StyleSheet,
-    View,
-    TextInput ,
-    Text,
-    ScrollView,
-    Image,
-    FlatList,
-    SectionList,
-    NavigatorIOS
+import React, { Component } from 'react';
+import { NavigatorIOS, Text, TouchableHighlight, View ,AppRegistry} from 'react-native';
+import PropTypes from 'prop-types';
 
-} from 'react-native';
-
-import React , {Component} from 'react'
-
-export default class FlexDirectionBasics extends  Component {
-
-    render(){
-        // console.log(getMovieFromApiAsync())
-        getMovies()
+export default class NavigatorIOSApp extends Component {
+    render() {
         return (
-            <View style={styles.container}>
-                <SectionList
-                    sections = {[
-                        {title:'d',data:['devin']},
-                        {title:'j',data:['jac','jdes','jasd','jsdfs']}
-                    ]}
-                    renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-                    renderSectionHeader ={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-                />
+            <NavigatorIOS
+        initialRoute={{
+            component: MyScene,
+                title: 'My Initial Scene',
+        }}
+        style={{flex: 1}}
+    />
+            
+        )
+    }
+}
+
+class MyScene extends Component {
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        navigator: PropTypes.object.isRequired,
+    }
+
+    constructor(props, context) {
+        super(props, context);
+        this._onForward = this._onForward.bind(this);
+    }
+
+    _onForward() {
+        this.props.navigator.push({
+            title: 'Scene ' + nextIndex,
+        });
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>Current Scene: { this.props.title }</Text>
+                <TouchableHighlight onPress={this._onForward}>
+                    <Text>Tap me to load the next scene</Text>
+                </TouchableHighlight>
             </View>
         )
     }
-
 }
 
-const  styles = StyleSheet.create({
-    container:{
-        flex:1,
-        paddingTop:22
-    },
-    sectionHeader:{
-        paddingTop:2,
-        paddingLeft:10,
-        paddingRight:10,
-        paddingBottom:2,
-        fontSize:14,
-        fontWeight:'bold',
-        backgroundColor:'red'
-    },
-    item:{
-      padding:10,
-      fontSize:18,
-      height:44,
-    }
-})
-
-function getMovieFromApiAsync(){
-    return fetch('https://facebook.github.io/react-native/movies.json')
-        .then((response) => response.json())
-        .then((responseJson) => {
-            console.log(responseJson.movies)
-            return responseJson.movies;
-        })
-        .catch((error) =>{
-            console.error(error)
-        })
-}
-
-function getMovies() {
-    let request = new XMLHttpRequest()
-
-
-    request.onreadystatechange = (e) => {
-        if (request.readyState != 4){
-            return;
-        }
-        if (request.status === 200){
-            console.log('success',request.responseText)
-        }else {
-            console.warn('error')
-        }
-    }
-    request.open('GET', 'https://mywebsite.com/endpoint/')
-    request.send()
-
-}
-AppRegistry.registerComponent("AwesomeProject",()=>FlexDirectionBasics)
+AppRegistry.registerComponent("AwesomeProject",()=>NavigatorIOSApp)
